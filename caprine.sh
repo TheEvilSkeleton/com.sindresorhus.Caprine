ef38ea51d82060315ed4b42ff6196c0b5fd5c20c
TheEvilSkeleton <theevilskeleton@riseup.net>
2020-11-23 11:28:03 -0500

    Revert "Don't portals because it's broken"
    This reverts commit 50996d5d47cbffd4f63b1b87535b47737dbff917.

--- caprine.sh
+++ caprine.sh
@@ -5,4 +5,4 @@ set -o errexit
 TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 export TMPDIR
 
-env TMPDIR=$XDG_CACHE_HOME CHROME_WRAPPER=$(readlink -f "$0") zypak-wrapper.sh /app/Caprine/caprine "$@"
+env TMPDIR=$XDG_CACHE_HOME CHROME_WRAPPER=$(readlink -f "$0") ZYPAK_FORCE_FILE_PORTAL=1 zypak-wrapper.sh /app/Caprine/caprine "$@"
